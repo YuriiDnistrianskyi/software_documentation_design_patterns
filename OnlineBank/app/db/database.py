d9fad1c47b3b8bac28d096312f84aa623f102b0e
YuraDnistriansky <yura.dniester@gmail.com>
2026-03-12 22:53:40 +0200

    init OnlineBank app

---
+++ OnlineBank/app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+class Base(DeclarativeBase):
+    pass
