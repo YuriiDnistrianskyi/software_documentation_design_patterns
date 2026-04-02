b717c233f07e7dd281df844da413b2359e44ce03
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-02 15:31:24 +0300

    fixs

--- OnlineBank/app/dal/dao/orders/cash_account_dao.py
+++ OnlineBank/app/dal/dao/orders/cash_account_dao.py
@@ -7,7 +7,7 @@ from app.db.database import CashAccount
 
 class CashAccountDAO(GeneralDAO[CashAccount]):
     async def get_cash_accounts_by_user_id(self, user_id: int, session: AsyncSession) -> list[CashAccount]:
-        stmt = select(CashAccount).where(CashAccount.user_id == user_id)
+        stmt = select(CashAccount).where(CashAccount.user_id == user_id).order_by(CashAccount.id)
         _list = await session.execute(stmt)
         result = list(_list.scalars().all())
 
