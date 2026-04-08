544a5f2f1ce21031c0024ca906bfd6acc4a7be84
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-08 14:35:45 +0300

    update config

--- lab_4/app/core/config.py
+++ lab_4/app/core/config.py
@@ -1,2 +1,7 @@
-DATA_SOURCE = ''
-SAVING_PLACE = 'this'
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATA_SOURCE = getenv('DATA_SOURCE')
+SAVING_PLACE = getenv('SAVING_PLACE')
