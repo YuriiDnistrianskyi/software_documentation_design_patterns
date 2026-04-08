56967859f20f82423c51e57e30d8952a9fcbf2c5
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-08 14:56:53 +0300

    add read_data_source

--- lab_4/app/core/config.py
+++ lab_4/app/core/config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATA_SOURCE = getenv('DATA_SOURCE')
+DATA_FILE_CSV = getenv('DATA_FILE_CSV')
 SAVING_PLACE = getenv('SAVING_PLACE')
--- lab_4/app/dal/app_dal.py
+++ lab_4/app/dal/app_dal.py
@@ -1,8 +1,23 @@
+import requests
+import csv
+
 from app.dal.i_app_dal import IAppDal
+from app.core.config import DATA_SOURCE, DATA_FILE_CSV
 
 class AppDal(IAppDal):
     def read_data_source(self):
-        print('read')
+        response = requests.get(DATA_SOURCE)
+        response.raise_for_status()
+
+        lines = response.text.splitlines()
+
+        reader = csv.reader(lines)
+
+        with open(DATA_FILE_CSV, 'w', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            for row in reader:
+                writer.writerow(row)
 
     def write_data(self):
         print('write')
